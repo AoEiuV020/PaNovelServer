98a9f69e7b257a3aace241902a3b472c5d81ee0a
AoEiuV020 <aoeiuv020@gmail.com>
2018-05-18 11:28:45 +0800

    确保待刷新列表能完整返回所有条目
    原先的如果有几百条请求失败的小说，就会堵在开头，
    改成一遍遍返回，刷完前不返回重复的，
    顺便改刷新时间为服务器端生成，因为客户端可能有时区和时间问题，
    顺便改判断更新的逻辑，只判断章节数，

--- src/main/kotlin/cc/aoeiuv020/panovel/server/service/impl/NovelServiceImpl.kt
+++ src/main/kotlin/cc/aoeiuv020/panovel/server/service/impl/NovelServiceImpl.kt
@@ -11,6 +11,8 @@ import cc.aoeiuv020.panovel.server.service.NovelService
 import cc.aoeiuv020.panovel.server.service.PushService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import java.util.*
+import java.util.concurrent.atomic.AtomicReference
 
 /**
  * Created by AoEiuV020 on 2018.04.05-10:07:57.
@@ -18,6 +20,19 @@ import org.springframework.stereotype.Service
  */
 @Service("novelService")
 class NovelServiceImpl : NovelService, BaseLoggable() {
+    companion object {
+        /**
+         * 记录当前表里刷新时间最新一条的时间，用于判断是否完整刷了一遍，
+         * 初始化为当前时间，因为之后刷新的都在这时间之后，
+         */
+        val refreshLatest: AtomicReference<Date> = AtomicReference(Date())
+        /**
+         * 表示刷新到哪个时间了，完整刷一遍前不重复刷新失败了的，
+         * 记录每次返回的待刷新列表最新一条的刷新时间，下次从这条开始，
+         */
+        val refreshNow: AtomicReference<Date> = AtomicReference(Date(0))
+    }
+
     @Autowired
     private lateinit var novelMapper: NovelMapper
     @Autowired
@@ -34,12 +49,30 @@ class NovelServiceImpl : NovelService, BaseLoggable() {
     }
 
     override fun needRefreshNovelList(count: Int): List<Novel> {
-        logger.info { "needRefreshNovelList count: $count" }
+        logger.info { "needRefreshNovelList count: $count, refreshTime: <${refreshLatest.get()}, ${refreshNow.get()}>" }
         require(count < 500)
-        val e = NovelExample().apply {
+        return novelMapper.selectByExample(NovelExample().apply {
             orderByClause = "modify_time asc limit $count"
+            or().andModifyTimeGreaterThan(refreshNow.get())
+        }).also {
+            it.lastOrNull()?.modifyTime?.also { nt ->
+                val lt = refreshLatest.get()
+                if (nt > lt) {
+                    // 如果取出的最后一个刷新时间大于保存的整张表最新刷新时间，
+                    // 表示刷完一遍，于是重置当前时间，从0开始，
+                    refreshNow.set(Date(0))
+                    refreshLatest.set(latestModifyTime())
+                } else {
+                    refreshNow.set(nt)
+                }
+            }
         }
-        return novelMapper.selectByExample(e)
+    }
+
+    private fun latestModifyTime(): Date {
+        return novelMapper.selectByExample(NovelExample().apply {
+            orderByClause = "modify_time desc limit 1"
+        }).firstOrNull()?.modifyTime ?: Date(0)
     }
 
     override fun uploadUpdate(novel: Novel): Boolean {
@@ -52,31 +85,22 @@ class NovelServiceImpl : NovelService, BaseLoggable() {
         if (result === novel) {
             return false
         }
-        // 如果数据库里的比较新，就不通知更新，
-        // 有的小说可能不知道更新时间，那就是默认，就会相等，对比chaptersCount,
-        if (novel.updateTime == null) {
-            if (result.chaptersCount >= novel.chaptersCount) {
-                return false
-            }
-        } else {
-            if (result.updateTime > novel.updateTime) {
-                return false
-            } else if (result.updateTime == novel.updateTime
-                    && result.chaptersCount >= novel.chaptersCount) {
-                return false
-            }
-        }
-        // 这里是确实需要更新的情况，
-        return true
+        // 有的小说可能不知道更新时间，那就是默认，未必会相等，时区没考虑,
+        // 所以只判断章节数，
+        return novel.chaptersCount > result.chaptersCount
     }
 
     /**
      * 更新小说，
+     * 如果比数据库里的新，也就是真的有更新，就推到极光，
+     * 否则只更新数据库，
+     * 也就是一定更新数据库，这样不怕无效更新，反正下次就覆盖了，
      *
      * @return 返回是否真的是有更新并成功更新数据库，
      */
     private fun updateActual(novel: Novel): Boolean {
         val hasUpdate = hasUpdate(novel)
+        novel.modifyTime = Date()
         if (hasUpdate) {
             pushService.pushUpdate(novel)
         }
