5c63c3fba448094575dbf2aefc75b4a084b0764f
AoEiuV020 <aoeiuv020@gmail.com>
2018-06-22 14:34:37 +0800

    给apache2装的php连接mysql不能写域名
    原因不明，改写死ip解决，

--- config.sample.ini.php
+++ config.sample.ini.php
@@ -3,7 +3,7 @@
  * Created by AoEiuV020 on 2018.06.22-09:23:10.
  */
 
-$ds->host = 'localhost';
+$ds->host = '127.0.0.1';
 $ds->username = 'panovel';
 $ds->password = 'panovel';
 $ds->database = 'panovel';
