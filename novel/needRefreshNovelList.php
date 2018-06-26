efbc895aa72d992b76c44b8fc6b1cac25cb39569
AoEiuV020 <aoeiuv020@gmail.com>
2018-06-26 18:15:14 +0800

    待刷新列表接口响应json中的变量名忘了换成驼峰
    sql拿出来的不能直接用，

--- novel/needRefreshNovelList.php
+++ novel/needRefreshNovelList.php
@@ -28,10 +28,14 @@ try {
 
     $stmt->bind_param('si', $lastTime, $limit);
     $stmt->execute();
+    $novelList = [];
     $arr = resultToArray($stmt->get_result());
+    foreach ($arr as $item) {
+        $novelList[] = new Novel($item);
+    }
 
-    if (count($arr) > 0) {
-        $last = new Novel($arr[count($arr) - 1]);
+    if (count($novelList) > 0) {
+        $last = $novelList[count($novelList) - 1];
         file_put_contents($lastTimeFile, $last->checkUpdateTime);
     } else {
         file_put_contents($lastTimeFile, 0);
@@ -39,9 +43,13 @@ try {
         $lastTime = 0;
         $stmt->bind_param('si', $lastTime, $limit);
         $stmt->execute();
+        $novelList = [];
         $arr = resultToArray($stmt->get_result());
-        if (count($arr) > 0) {
-            $last = new Novel($arr[count($arr) - 1]);
+        foreach ($arr as $item) {
+            $novelList[] = new Novel($item);
+        }
+        if (count($novelList) > 0) {
+            $last = $novelList[count($novelList) - 1];
             file_put_contents($lastTimeFile, $last->checkUpdateTime);
         }
     }
@@ -49,7 +57,7 @@ try {
 
     error_log("query refresh list later than $lastTime count: " . count($arr));
 
-    success($arr);
+    success($novelList);
 } catch (Throwable $e) {
     catchException($e);
 }
